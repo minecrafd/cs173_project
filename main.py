d6824688dba8dd6e72570581a8fff4b36fd6a390
minecrafd <95729621+minecrafd@users.noreply.github.com>
2024-04-10 01:41:11 +0800

    fix chinese bug

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from bilibili_api import rank, sync
+from bilibili_api import rank, sync, video
 import asyncio
 import os
 import json
@@ -23,18 +23,24 @@ async def example():
         result = await rank.get_rank(type_=member)
         os.makedirs(f'./data/{member_name}', exist_ok=True)
         index = 0
-        for video in result['list']:
-            video_name = video['title']
+        for video_item in result['list']:
+
+            video_name = video_item['title']
+            cid = video_item['cid']
+            aid = video_item['aid']
+            vid = video.Video(aid=aid, credential=credential)
+            subtitles = await vid.get_subtitle(cid=cid)
+            hashtags = await vid.get_tags()
             comments = []
             processed_comments = []
             # 页码
             # 当前已获取数量
             count = 0
-            c = await comment.get_comments(video['stat']['aid'], comment.CommentResourceType.VIDEO, 1,
+            c = await comment.get_comments(video_item['stat']['aid'], comment.CommentResourceType.VIDEO, 1,
                                            credential=credential)
-            for page in tqdm(range(1, c["page"]["count"] // 20), desc='Processing'):
+            for page in tqdm(range(1, min(c["page"]["count"] // 20 - 6, 200)), desc='Processing'):
                 # 获取评论
-                c = await comment.get_comments(video['stat']['aid'], comment.CommentResourceType.VIDEO, page, credential=credential)
+                c = await comment.get_comments(video_item['stat']['aid'], comment.CommentResourceType.VIDEO, page, credential=credential)
                 l = c['replies']
                 if l is None:
                     break
@@ -51,10 +57,16 @@ async def example():
             # 处理评论
             for cmt in comments:
                 processed_comments.append({'content': cmt['content']['message'], 'like': cmt['like']})
-            dic = {'title': video_name, 'stat': video['stat'], 'comments': processed_comments}
+            dic = {'title': video_name, 'stat': video_item['stat'], 'comments': processed_comments, 'cid': cid, 'hashtag': hashtags, 'subtitles': subtitles}
             with open(f'./data/{member_name}/{index}.json', 'w', encoding='utf-8') as json_file:
-                json.dump(dic, json_file)
+                json.dump(dic, json_file, ensure_ascii=False)
+            index += 1
 
+async def example2():
+    vid = video.Video(bvid='BV1BH4y1n7Mn', credential=credential)
+    result = await vid.get_info()
+    result = await vid.get_subtitle(cid=1496143412)
+    print(result)
 asyncio.run(example())
 
 
