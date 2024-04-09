3883200db0388ef7f1204a8fea8561a597471d9f
minecrafd <95729621+minecrafd@users.noreply.github.com>
2024-04-10 00:39:48 +0800

    add tqdm on main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import asyncio
 import os
 import json
 from bilibili_api import comment, sync, Credential, settings
+from tqdm import tqdm
+
 
 settings.proxy = 'http://luowubeigui.tpddns.cn:10086'
 member_names = ["All", "Bangumi", "GuochuangAnime", "Guochuang", "Documentary",
@@ -26,10 +28,11 @@ async def example():
             comments = []
             processed_comments = []
             # 页码
-            page = 1
             # 当前已获取数量
             count = 0
-            while True:
+            c = await comment.get_comments(video['stat']['aid'], comment.CommentResourceType.VIDEO, 1,
+                                           credential=credential)
+            for page in tqdm(range(1, c["page"]["count"] // 20), desc='Processing'):
                 # 获取评论
                 c = await comment.get_comments(video['stat']['aid'], comment.CommentResourceType.VIDEO, page, credential=credential)
                 l = c['replies']
@@ -40,10 +43,8 @@ async def example():
                 # 增加已获取数量
                 count += c['page']['size']
                 # 增加页码
-                page += 1
-                await asyncio.sleep(0.5)
-
-                if count >= c['page']['count'] - 100:
+                await asyncio.sleep(0.3)
+                if count >= c['page']['count'] - 20:
                     # 当前已获取数量已达到评论总数，跳出循环
                     break
 
@@ -51,7 +52,7 @@ async def example():
             for cmt in comments:
                 processed_comments.append({'content': cmt['content']['message'], 'like': cmt['like']})
             dic = {'title': video_name, 'stat': video['stat'], 'comments': processed_comments}
-            with open(f'./data/{member_name}/{index}.json', 'w') as json_file:
+            with open(f'./data/{member_name}/{index}.json', 'w', encoding='utf-8') as json_file:
                 json.dump(dic, json_file)
 
 asyncio.run(example())
