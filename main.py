9f1129e0e8609fda7ba4dd231c397292432a4f91
mike3090 <mike3090@163.com>
2024-04-13 16:00:55 +0800

    new data!

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from tqdm import tqdm
 
 
 # settings.proxy = 'http://luowubeigui.tpddns.cn:10086'
-member_names = ["Douga", "Music", "Dance", "Game", "Knowledge", "Technology",
-                    "Sports", "Life", "Food", "Animal",
-                    "Fashion", "Ent", "Movie"]
-credential = Credential(sessdata="495336ff%2C1728185870%2Cea1aa%2A42CjCHi9VdJWNmUeK2WX9YoHIl-yCBjhc5rNACQvqMG7YVCMeNpCjGoPAf-08hGEmmMEESVm1nNmw1NHdpQ3I4ZldaTWRjbXBNRmI1WVFyVUdmRkxiYjBtdE9xTG5QUFRWUnZXQ1lSamxjRngzOUdKQzdyRWJIR19qalNqYmc5dnFzbmFhVkVCYmFRIIEC", 
-                        bili_jct="216a9cb665e2838ad16414b45599f602", ac_time_value="5a0c0f6f28cc4d1717606d8108b33342")
+# member_names = ["Douga", "Music", "Dance", "Game", "Knowledge", "Technology",
+#                     "Sports", "Life", "Food", "Animal",
+#                     "Fashion", "Ent", "Movie"]
+member_names = ["All"]
+credential = Credential(sessdata="38259e0c%2C1728457244%2C034de%2A42CjDLfIjnB-qE7TmXtONb8Kju2SRhy8nqBZ4A8-vAFU2P8tquaH8AqZO3L-yc2P5KUdcSVk5LcWFfZnQySFZUZ0tJRkdKYnZYelRCWEJ3QmdsV0NjZlg3UFVpTWpGb2dnemoydk00dUpIb185MTJSX09sMVlURE11MDlOZHJkWG5TZ2FKakxXTXR3IIEC", 
+                        bili_jct="9ea610c9f466855e31f4ceb07d1edbaf", ac_time_value="ad2cc5aa5dc70c2aefb3b83f64c39442")
 
 async def example():
 
@@ -20,7 +21,7 @@ async def example():
     for member_name in member_names:
         member = eval("rank.RankType." + member_name)
         result = await rank.get_rank(type_=member)
-        os.makedirs(f'./data/{member_name}', exist_ok=True)
+        os.makedirs(f'./data2/{member_name}', exist_ok=True)
         index = 0
         for video_item in result['list']:
             try:
@@ -28,6 +29,8 @@ async def example():
                 cid = video_item['cid']
                 aid = video_item['aid']
                 vid = video.Video(aid=aid, credential=credential)
+                desc = await vid.get_info()
+                desc = desc["desc"]
                 subtitles = await vid.get_subtitle(cid=cid)
                 hashtags = await vid.get_tags()
                 comments = []
@@ -35,29 +38,36 @@ async def example():
                 # 页码
                 # 当前已获取数量
                 count = 0
-                c = await comment.get_comments(video_item['stat']['aid'], comment.CommentResourceType.VIDEO, 1,
+                c = await comment.get_comments(video_item['stat']['aid'], comment.CommentResourceType.VIDEO, 1, order=comment.OrderType.LIKE,
                                                 credential=credential)
                 for page in tqdm(range(1, min(c["page"]["count"] // 20 - 1, 26)), desc='Processing'):
+                # for page in tqdm(range(1, c["page"]["count"] // 20 - 1), desc='Processing'):
+                    # print(c["page"])
                     # 获取评论
-                    c = await comment.get_comments(video_item['stat']['aid'], comment.CommentResourceType.VIDEO, page, credential=credential)
-                    l = c['replies']
-                    if l is None:
+                    try:
+                        c = await comment.get_comments(video_item['stat']['aid'], comment.CommentResourceType.VIDEO, page, order=comment.OrderType.LIKE, credential=credential)
+                    except Exception as e:
+                        print(e)
                         break
-                    # 存储评论
-                    comments.extend(l)
-                    # 增加已获取数量
-                    count += c['page']['size']
-                    # 增加页码
-                    await asyncio.sleep(0.3)
-                    if count >= c['page']['count'] - 20:
-                        # 当前已获取数量已达到评论总数，跳出循环
-                        break
+                    else:
+                        l = c['replies']
+                        if l is None:
+                            break
+                        # 存储评论
+                        comments.extend(l)
+                        # 增加已获取数量
+                        count += c['page']['size']
+                        # 增加页码
+                        await asyncio.sleep(0.3)
+                        if count >= c['page']['count']:
+                            # 当前已获取数量已达到评论总数，跳出循环
+                            break
 
                 # 处理评论
                 for cmt in comments:
                     processed_comments.append({'content': cmt['content']['message'], 'like': cmt['like']})
-                dic = {'title': video_name, 'stat': video_item['stat'], 'comments': processed_comments, 'cid': cid, 'hashtag': hashtags, 'subtitles': subtitles}
-                with open(f'./data/{member_name}/{index}.json', 'w', encoding='utf-8') as json_file:
+                dic = {'title': video_name, 'stat': video_item['stat'], 'desc': desc, 'cid': cid, 'hashtag': hashtags, 'subtitles': subtitles, 'comments': processed_comments}
+                with open(f'./data2/{member_name}/{index}.json', 'w', encoding='utf-8') as json_file:
                     json.dump(dic, json_file, ensure_ascii=False)
                 
             except KeyboardInterrupt:
@@ -70,10 +80,10 @@ async def example():
                 index += 1
 
 async def example2():
-    vid = video.Video(bvid='BV1BH4y1n7Mn', credential=credential)
+    vid = video.Video(bvid='BV1yA4m1F7cP', credential=credential)
     result = await vid.get_info()
-    result = await vid.get_subtitle(cid=1496143412)
-    print(result)
+    # result = await vid.get_subtitle(cid=1496143412)
+    print(result["desc"])
 asyncio.run(example())
 
 
